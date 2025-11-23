f3ba08ba59c14b959eb146c69a67bb6e0421375d
njabulobc <156068484+njabulobc@users.noreply.github.com>
2025-11-23 11:08:44 +0200

    Update settings.py

--- ZisoDB/ZisoDB/settings.py
+++ ZisoDB/ZisoDB/settings.py
@@ -27,11 +27,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = [
-    host.strip()
-    for host in os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
-    if host.strip()
-]
+ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     origin.strip()
